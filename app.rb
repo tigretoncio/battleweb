f350615544254a57c7ed9fad7153bfc7140e8728
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-04-27 14:00:59 +0100

    Step 19

--- app.rb
+++ app.rb
@@ -4,13 +4,21 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
+enable :sessions
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @player1  = params[:player_1_name]
-    @player2 = params[:player_2_name]
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect ('/play')
+  end
+
+  get '/play' do
+    @player1 = session[:player_1_name]
+    @player2 = session[:player_2_name]
     erb(:play)
   end
 
@@ -18,3 +26,11 @@ class Battle < Sinatra::Base
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
+
+
+ # Use enable to enable the session in Sinatra
+ # In the post '/names' route, store the player names in the session
+ # Write a get '/play route that renders the play.erb view you already wrote
+ # In the get '/play' route, extract the instance variables required by the view from the session
+ # Remove the erb :play expression from the post '/names' route, and replace it with a redirect to the '/play' route
+ # Ensure your enter_names_spec feature test still passes.
