9b51a7ab9cc26497e13b523a5f968cdd565130ee
Max Provin <maxprovin@outlook.com>
2016-04-27 16:19:12 +0100

    Step 23 extracting logic to the model

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require 'sinatra/base'
-
+require 'player'
 
 
 class Battle < Sinatra::Base
@@ -11,20 +11,20 @@ enable :sessions
   end
 
   post '/names' do
-    session[:player_1_name] = params[:player_1_name]
-    session[:player_2_name] = params[:player_2_name]
+    $player1 = Player.new(params[:player_1_name])
+    $player2 = Player.new(params[:player_2_name])
     redirect ('/play')
   end
 
   get '/play' do
-    @player1 = session[:player_1_name]
-    @player2 = session[:player_2_name]
+    @player1 = $player1.name
+    @player2 = $player2.name
     erb(:play)
   end
 
   get '/attack' do
-    @player1 = session[:player_1_name]
-    @player2 = session[:player_2_name]
+    @player1 = $player1.name
+    @player2 = $player2.name
     erb(:attack)
   end
 
