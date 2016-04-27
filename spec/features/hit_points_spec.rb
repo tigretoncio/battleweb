bfd64fe8d52a3cf36c5c15576505c87918e9c1a3
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-04-27 14:20:16 +0100

    step 21

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,10 +1,6 @@
 feature "Hit Points", :type => :feature do
   scenario "hit points displayed" do
-    visit "/"
-    fill_in "player_1_name", :with => "Sergio"
-    fill_in "player_2_name", :with => "Max"
-    click_button "Play"
-
+    sign_in_and_play
     expect(page).to have_text("Hit points: 25")
   end
 end
--- spec/features/name_registration_spec.rb
+++ spec/features/name_registration_spec.rb
@@ -1,11 +1,6 @@
 feature "Name Registration", :type => :feature do
   scenario "Form available for player names" do
-    visit "/"
-
-    fill_in "player_1_name", :with => "Sergio"
-    fill_in "player_2_name", :with => "Max"
-    click_button "Play"
-
+    sign_in_and_play
     expect(page).to have_text("Sergio vs Max FIGHT!")
   end
 end
